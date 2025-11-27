3b085a5d159130aaab8bb450642e723274b5c464
saimm <saimshehzad2030@gmail.com>
2025-11-27 18:47:00 +0500

    few changes

---
+++ dist/index.js
@@ -0,0 +1,38 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(body_parser_1.default.json({
+    verify: (req, res, buf) => {
+        req.body = buf;
+    },
+}));
+const port = process.env.PORT || 3000;
+app.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        res.status(200).json({ message: 'Users fetched' });
+    }
+    catch (error) {
+        res.status(500).json({ error: error });
+    }
+}));
+app.listen(port, () => __awaiter(void 0, void 0, void 0, function* () {
+    console.log(`Server is running on http://localhost:${port} `);
+}));
+exports.default = app;
+//# sourceMappingURL=index.js.map
